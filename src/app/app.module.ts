81f7c79c232482d702b7570facafa7db31e65bcf
OscarGordon <oscargordon@Mac-mini-de-Oscar.local>
2017-05-23 22:05:44 +0200

    initial commit
    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,52 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
+import { ItemDetailsPage } from '../pages/item-details/item-details';
+import { ListPage } from '../pages/list/list';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+// Import the AngularFire2 module
+import { AngularFireModule } from 'angularfire2';
+
+// AngularFire2 settings
+export const firebaseConfig = {
+  apiKey: "AIzaSyCDZyYbAze84Vfqw627aB-goromg7iVhaY",
+  authDomain: "norep-3a7a5.firebaseapp.com",
+  databaseURL: "https://norep-3a7a5.firebaseio.com",
+  projectId: "norep-3a7a5",
+  storageBucket: "norep-3a7a5.appspot.com",
+  messagingSenderId: "920657672761"
+};
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HelloIonicPage,
+    ItemDetailsPage,
+    ListPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    // angularfiremodule initialization
+    AngularFireModule.initializeApp(firebaseConfig)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HelloIonicPage,
+    ItemDetailsPage,
+    ListPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-hello-ionic',
+  templateUrl: 'hello-ionic.html'
+})
+export class HelloIonicPage {
+    constructor() {    
+  }  
+}
